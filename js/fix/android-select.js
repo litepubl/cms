19136a2e0326a235dd38690d2ec76792b101c2d0
litepubl <yarrowsoft@gmail.com>
2015-04-23 12:43:58 +0300

    mini fix/android-select.js

--- js/fix/android-select.js
+++ js/fix/android-select.js
@@ -9,8 +9,8 @@
   'use strict';
 
 $(document).ready(function() {
-  var nua = navigator.userAgent
-  var isAndroid = (nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android ') > -1 && nua.indexOf('AppleWebKit') > -1 && nua.indexOf('Chrome') === -1)
+  var nua = navigator.userAgent;
+  var isAndroid = (nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android ') > -1 && nua.indexOf('AppleWebKit') > -1 && nua.indexOf('Chrome') === -1);
   if (isAndroid) {
 $.fn.fixAndroid = function() {
     this.find('select.form-control').removeClass('form-control').css('width', '100%');
