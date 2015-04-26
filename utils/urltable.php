fad8bdb5644ec2baec5f0d22cd59b65ff7b45505
litepubl <yarrowsoft@gmail.com>
2015-04-26 16:12:26 +0300

    deprecated utils/deprecated/auth.php

--- utils/urltable.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('litepublisher_mode', 'xmlrpc');
-include('index.php');
-litepublisher::$urlmap->loadall();
-foreach (litepublisher::$urlmap->items as $id => $item) {
-echo $item['url'], ': ', $item['class'], '<br>';
-}
-?>
--- utils/widgetslist.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('litepublisher_mode', 'xmlrpc');
-include('index.php');
-echo "<pre>\n";
-$widgets = twidgets::instance();
-var_dump($widgets->data);
-?>
