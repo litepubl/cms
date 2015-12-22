fdf6e54237b67ca41461e2a4e9ce42fe42ecfd30
litepubl <yarrowsoft@gmail.com>
2015-12-22 22:31:38 +0300

    final

--- lib/update/update.6.03.php
+++ lib/update/update.6.03.php
@@ -2,6 +2,6 @@
 function update603() {
 $classes = litepublisher::$classes;
 $classes->items['themevars'] = array('kernel.templates.php', '', 'theme.vars.class.php');
-$classes->add('tablebuilder, 'kernel.admin.php', '', 'html.tablebuilder.class.php');
+$classes->items['tablebuilder'] = array('kernel.admin.php', '', 'html.tablebuilder.class.php');
 $classes->save();
 }
