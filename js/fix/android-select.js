03c1bee20c6817e50dbc2cdf69f6fca3adb5a190
litepubl <yarrowsoft@gmail.com>
2016-01-17 23:37:38 +0300

    reparsed all

--- js/fix/android-select.js
+++ js/fix/android-select.js
@@ -1,28 +1,28 @@
 /**
-* Lite Publisher
-* Copyright (C) 2010 - 2015 Vladimir Yushko http://litepublisher.ru/ http://litepublisher.com/
-* Dual licensed under the MIT (mit.txt)
-* and GPL (gpl.txt) licenses.
-**/
+ * Lite Publisher
+ * Copyright (C) 2010 - 2015 Vladimir Yushko http://litepublisher.ru/ http://litepublisher.com/
+ * Dual licensed under the MIT (mit.txt)
+ * and GPL (gpl.txt) licenses.
+ **/
 
-(function ($, document) {
+(function($, document) {
   'use strict';
 
-$(document).ready(function() {
-  var nua = navigator.userAgent;
-  var isAndroid = (nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android ') > -1 && nua.indexOf('AppleWebKit') > -1 && nua.indexOf('Chrome') === -1);
-  if (isAndroid) {
-$.fn.fixAndroid = function() {
-    this.find('select.form-control').removeClass('form-control').css('width', '100%');
-return this;
-};
+  $(document).ready(function() {
+    var nua = navigator.userAgent;
+    var isAndroid = (nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android ') > -1 && nua.indexOf('AppleWebKit') > -1 && nua.indexOf('Chrome') === -1);
+    if (isAndroid) {
+      $.fn.fixAndroid = function() {
+        this.find('select.form-control').removeClass('form-control').css('width', '100%');
+        return this;
+      };
 
-$("body").fixAndroid();
-} else {
-$.fn.fixAndroid = function() {
-return this;
-};
-  }
+      $("body").fixAndroid();
+    } else {
+      $.fn.fixAndroid = function() {
+        return this;
+      };
+    }
 
-});
+  });
 }(jQuery, document));
--- js/fix/ie10.js
+++ js/fix/ie10.js
@@ -1,16 +1,16 @@
 /**
-* Lite Publisher
-* Copyright (C) 2010 - 2015 Vladimir Yushko http://litepublisher.ru/ http://litepublisher.com/
-* Dual licensed under the MIT (mit.txt)
-* and GPL (gpl.txt) licenses.
-**/
+ * Lite Publisher
+ * Copyright (C) 2010 - 2015 Vladimir Yushko http://litepublisher.ru/ http://litepublisher.com/
+ * Dual licensed under the MIT (mit.txt)
+ * and GPL (gpl.txt) licenses.
+ **/
 
-(function ($, document) {
+(function($, document) {
   'use strict';
 
   if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
-$(document).ready(function() {
-$('<style type="text/css">@-ms-viewport{width:auto!important}</style>').appendTo("head:first");
-});
-}
+    $(document).ready(function() {
+      $('<style type="text/css">@-ms-viewport{width:auto!important}</style>').appendTo("head:first");
+    });
+  }
 }(jQuery, document));
