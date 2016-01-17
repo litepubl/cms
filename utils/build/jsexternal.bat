840558c4d0d7b99f8f26c6f5f5da6414a266c9bf
litepubl <yarrowsoft@gmail.com>
2016-01-17 20:56:00 +0300

    css less

--- utils/build/jsexternal.bat
+++ utils/build/jsexternal.bat
@@ -1,5 +1,9 @@
 SETLOCAL  
 set home=d:\OpenServer\domains\cms
 for /f %%f in (jsexternal.txt) do java -jar closure\compiler.jar --js=%home%\js\%%f.js --js_output_file=%home%\js\%%f.min.js
-
+call jsfile %home%\js\fix\android-select
+call jsfile %home%\js\fix\ie10
+cd %home%\js\fix
+copy html5shiv.js + respond.src.js ie9.js /b
+copy html5shiv.min.js + respond.min.js ie9.min.js /b
 ENDLOCAL   
--- utils/build/jsplugins.bat
+++ utils/build/jsplugins.bat
@@ -1,5 +1,5 @@
 SETLOCAL  
 set home=d:\OpenServer\domains\cms
-node D:\OpenServer\modules\node_modules\less\bin\lessc %home%\plugins\photoswipe\resource\default-skin\default-skin.inline.less %home%\plugins\photoswipe\resource\default-skin\default-skin.inline.css
+for /f %%f in (jsless.txt) do node D:\OpenServer\modules\node_modules\less\bin\lessc %home%\%%f.less %home%\%%f.css
 for /f %%f in (jsplugins.txt) do call jsfile %home%\plugins\%%f
 ENDLOCAL   
