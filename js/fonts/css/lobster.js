7385afae823dd008baa37dafabee1461035c6326
litepubl <yarrowsoft@gmail.com>
2016-02-21 12:11:02 +0300

    loader font

--- js/fonts/css/lobster.js
+++ js/fonts/css/lobster.js
@@ -9,33 +9,7 @@
   'use strict';
 
   $.load_lobster = function() {
-    //$.load_css("http://fonts.googleapis.com/css?family=Lobster&subset=latin,cyrillic");
-    var css = "@font-face {" +
-      "font-family: 'Lobster';" +
-      "font-style: normal;" +
-      "font-weight: 400;" +
-      "src: url('%s.eot');" +
-      "src: local('Lobster'), local('Lobster-Regular'), " +
-      "url('%s.eot?#iefix') format('embedded-opentype')," +
-      "url('%s.woff') format('woff')," +
-      "url('%s.woff2') format('woff2')," +
-      "url('%s.ttf') format('truetype')," +
-      "url('%s.svg#Lobster') format('svg');" +
-      "}";
-
-    css = css.replace(/%s/gm, ltoptions.files + '/js/fonts/lobster/lobster');
-
-    $.css_loader.addtext(css);
-
-    // sometimes lobster not detected without timeout
-    setTimeout(function() {
-      var observer = new FontFaceObserver('Lobster', {
-        weight: 400
-      });
-
-      observer.check().then(function() {
-        $("body").addClass("lobster");
-      });
-    }, 0);
+$.load_font_part('Lobster', 'lobster', ltoptions.files + '/js/fonts/lobster/lobster');
   };
+
 })(jQuery);
--- js/litepubl/system/load-font.js
+++ js/litepubl/system/load-font.js
@@ -9,12 +9,45 @@
 
   $.load_font = function(font_name, class_name, css_url) {
     $.load_css(css_url);
+$.wait_font(font_name, class_name);
+};
+
+  $.load_font_part = function(font_name, class_name, part_url) {
+var css = $.get_font_face(font_name, part_url);
+    $.css_loader.addtext(css);
+$.wait_font(font_name, class_name);
+};
+
+  $.wait_font = function(font_name, class_name) {
+    // sometimes font not detected without timeout
+    setTimeout(function() {
     var observer = new FontFaceObserver(font_name, {
       weight: 400
     });
+
     observer.check().then(function() {
       $("body").addClass(class_name);
     });
+}, 0);
+  };
+
+$.get_font_face = function(font_name, part_url) {
+    var css = "@font-face {" +
+      "font-family: '%name%';" +
+      "font-style: normal;" +
+      "font-weight: 400;" +
+      "src: url('%url%.eot');" +
+      "src: local('%name%'), local('%name%-Regular'), " +
+      "url('%url%.eot?#iefix') format('embedded-opentype')," +
+      "url('%url%.woff') format('woff')," +
+      "url('%url%.woff2') format('woff2')," +
+      "url('%url%.ttf') format('truetype')," +
+      "url('%url%.svg#%name%') format('svg');" +
+      "}";
+
+    css = css.replace(/%url%/gm, part_url);
+    css = css.replace(/%name%/gm, font_name);
+return css;
   };
 
 }(jQuery));
