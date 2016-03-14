0eaaf6eca3e64c910a0a28a07c4484e846f546d8
litepubl <yarrowsoft@gmail.com>
2016-03-14 22:11:15 +0300

    css fix

--- lib/update/update.6.08.php
+++ lib/update/update.6.08.php
@@ -1,3 +1,9 @@
 <?php
 function update608() {
+$css = tcssmerger::i();
+$css->replacefile('default',
+'/js/litepublisher/css/form-inline.min.css',
+'/js/litepubl/common/css/form.inline.min.css'
+);
+
 }
