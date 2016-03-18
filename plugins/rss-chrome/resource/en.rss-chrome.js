b11579181df73d611aa30a1da326c7ee4d1d37da
litepubl <yarrowsoft@gmail.com>
2016-03-18 23:29:11 +0300

    before source

---
+++ plugins/rss-chrome/resource/en.rss-chrome.js
@@ -0,0 +1,13 @@
+/**
+* Lite Publisher
+* Copyright (C) 2010 - 2016 Vladimir Yushko http://litepublisher.com/ http://litepublisher.ru/
+* Licensed under the MIT (LICENSE.txt) license.
+**/
+
+(function(window) {
+  window.lang = window.lang || {};
+  lang.rsschrome = {
+    warn: "Warning! By default Chrome doesnt not  support RSS. Please install extension and press continue",
+    follow: "Continue"
+  };
+})(window);
