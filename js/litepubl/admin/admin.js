120a751eda0b2191339d713abc401598a01d3ae2
litepubl <yarrowsoft@gmail.com>
2016-04-06 22:09:48 +0300

    js mini

--- js/litepubl/admin/admin.js
+++ js/litepubl/admin/admin.js
@@ -1,8 +1,8 @@
 /**
-* Lite Publisher
-* Copyright (C) 2010 - 2016 Vladimir Yushko http://litepublisher.com/ http://litepublisher.ru/
-* Licensed under the MIT (LICENSE.txt) license.
-**/
+ * Lite Publisher
+ * Copyright (C) 2010 - 2016 Vladimir Yushko http://litepublisher.com/ http://litepublisher.ru/
+ * Licensed under the MIT (LICENSE.txt) license.
+ **/
 
 (function($, document) {
   'use strict';
