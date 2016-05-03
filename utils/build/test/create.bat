21134aab2eab31fa52401df6191e7126e9ac463c
litepubl <yarrowsoft@gmail.com>
2016-05-03 12:59:59 +0300

    added test init

---
+++ utils/build/test/create.bat
@@ -0,0 +1,5 @@
+rem @ECHO OFF
+setlocal DISABLEDELAYEDEXPANSION
+@set PHP_DIR=D:\OpenServer\modules\php\PHP-7-x64\
+SET BIN_TARGET=%~dp0/vendor/codeception/codeception/codecept
+%php_dir%php.exe "%BIN_TARGET%" %*
