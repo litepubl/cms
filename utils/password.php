3b16132830d9b4bfef19f002c809edcbc7da18c9
litepubl <yarrowsoft@gmail.com>
2016-07-04 18:55:48 +0300

    move to rc

--- utils/password.php
+++ utils/password.php
@@ -4,11 +4,12 @@
     error_reporting(E_ALL | E_NOTICE | E_STRICT | E_WARNING );
     ini_set('display_errors', 1);
 
-define('litepublisher_mode', 'xmlrpc');
+define('litepubl\mode', 'config');
 include('index.php');
+
    $password = md5uniq();
-   litepublisher::$options->changepassword($password);
-litepublisher::$options->savemodified();
+   litepubl::$app->options->changepassword($password);
+   litepubl::$app->poolStorage->commit();
 echo "<pre>\n";
-echo litepublisher::$options->email;
+echo litepubl::$app->options->email;
 echo "\n$password\n<br>new password";
