74d1682cba01fb4aae532109a08ab54b5bbc8f7c
litepubl <yarrowsoft@gmail.com>
2016-07-08 11:30:13 +0300

    events trait in progresss

--- lib/core/Events.php
+++ lib/core/Events.php
@@ -79,55 +79,6 @@ use EventsTrait;
         }
     }
 
-    public function eventExists(string $name): bool
-    {
-        return in_array($name, $this->eventnames);
-    }
-
-    public function __get($name)
-    {
-        if (method_exists($this, $name)) {
-            return [get_class($this) ,                 $name];
-        }
-
-        return parent::__get($name);
-    }
-
-    public function __set($name, $value)
-    {
-        if (parent::__set($name, $value)) {
-            return true;
-        }
-
-        $eventName = strtolower($name);
-        if (in_array($eventName, $this->eventnames)) {
-            $this->addEvent($eventName, $value[0], $value[1]);
-            return true;
-        }
-
-            throw new PropException(get_class($this), $name);
-    }
-
-    public function method_exists(string $name): bool
-    {
-        return in_array($name, $this->eventnames);
-    }
-
-    public function __call($name, $params)
-    {
-        $eventName = strtolower($name);
-        if (in_array($eventName, $this->eventnames)) {
-            return $this->callEvent($eventName, $params);
-        }
-
-        return parent::__call($name, $params);
-    }
-
-    public function __isset($name)
-    {
-        return parent::__isset($name) || in_array($name, $this->eventnames);
-    }
-
     protected function addEvents()
     {
         $a = func_get_args();
--- lib/core/EventsTrait.php
+++ lib/core/EventsTrait.php
@@ -6,4 +6,53 @@ trait EventsTrait
 {
 use Callbacks;
 
+    public function __get($name)
+    {
+        if (method_exists($this, $name)) {
+            return [get_class($this) ,                 $name];
+        }
+
+        return parent::__get($name);
+    }
+
+    public function __set($name, $value)
+    {
+        if (parent::__set($name, $value)) {
+            return true;
+        }
+
+        $eventName = strtolower($name);
+        if (in_array($eventName, $this->eventnames)) {
+            $this->addEvent($eventName, $value[0], $value[1]);
+            return true;
+        }
+
+            throw new PropException(get_class($this), $name);
+    }
+
+    public function __call($name, $params)
+    {
+        $eventName = strtolower($name);
+        if (in_array($eventName, $this->eventnames)) {
+            return $this->callEvent($eventName, $params);
+        }
+
+        return parent::__call($name, $params);
+    }
+
+    public function __isset($name)
+    {
+        return parent::__isset($name) || in_array($name, $this->eventnames);
+    }
+
+    public function eventExists(string $name): bool
+    {
+        return in_array($name, $this->eventnames);
+    }
+
+    public function method_exists(string $name): bool
+    {
+        return in_array($name, $this->eventnames);
+    }
+
 }
