0bc3c7312fa98e33bedd9dfe6c67a75f538e9780
litepubl <yarrowsoft@gmail.com>
2016-07-23 12:17:27 +0300

    release 7.01-3

--- utils/build/test/c.bat
+++ utils/build/test/c.bat
@@ -1,8 +1,8 @@
 del tests\_output\*.* /f /q
 del tests\_output\debug\*.* /f /q
-@rem vendor\bin\codecept.bat run acceptance  --steps --html --debug>bresult.txt
+vendor\bin\codecept.bat run acceptance  --steps --html --debug>bresult.txt
 @rem vendor\bin\codecept.bat run acceptance  09homeCept.php --steps --html --debug>bresult.txt
 @rem vendor\bin\codecept.bat run acceptance 05AdminCept.php --steps --html --debug>bresult.txt
 @rem codecept.bat run acceptance 06EditorCept.php --steps --html --debug>bresult.txt
 @rem codecept.bat run api 01rssCept.php --steps --html --debug>bresult.txt
-vendor\bin\codecept.bat run acceptance 20uloginCept.php --steps --html --debug>bresult.txt
+@ren vendor\bin\codecept.bat run acceptance 20uloginCept.php --steps --html --debug>bresult.txt
