bc1a447acfc5ce634281c7b58ca7e47aaba6444e
litepubl <yarrowsoft@gmail.com>
2016-08-25 23:39:06 +0300

    test update final

--- utils/build/test/tests/updateftp/30updateCept.php
+++ utils/build/test/tests/updateftp/30updateCept.php
@@ -13,15 +13,28 @@ use Page\Service;
 $i = new AcceptanceTester($scenario);
 $i->wantTo('Test update');
 $service = new Service($i, '30update');
+$i->wantTo('Decrement current version');
 $service->open($service->runUrl);
 $i->fillField($service->runText, $service->runFixture);
 $service->screenShot('runscript');
 $service->submit();
-return;
+
+$i->wantTo('Fill account form');
 $service->open($service->url);
 $i->fillField($service->hostText, $service->hostFixture);
 $i->fillField($service->loginText, $service->loginFixture);
 $i->fillField($service->passwordText, $service->passwordFixture);
+$service->screenshot('ftpaccount');
 $i->click($service->autoButton);
+$i->wantTo('Wait finish update');
         $i->waitForElement('table', 300);
-$i->checkError();
+// exclude warning from $i->checkError();
+$i->wantTo('Check errors');
+$text = htmlspecialchars_decode($i->getVisibleText());
+foreach (['exception', 'Parse error', 'Fatal error', 'Notice: Undefined'] as $err) {
+if (strpos($text, $err) !== false) {
+$i->assertTrue(false, $err);
+}
+}
+
+$service->screenshot('updated');
