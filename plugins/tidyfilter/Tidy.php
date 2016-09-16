0e670e73ff6b1225530a3b15b3d2682170f69ade
litepubl <yarrowsoft@gmail.com>
2016-09-16 11:42:34 +0300

    coprights updated

--- plugins/tidyfilter/Tidy.php
+++ plugins/tidyfilter/Tidy.php
@@ -5,7 +5,7 @@
  * @copyright 2010 - 2016 Vladimir Yushko http://litepublisher.com/ http://litepublisher.ru/
  * @license   https://github.com/litepubl/cms/blob/master/LICENSE.txt MIT
  * @link      https://github.com/litepubl\cms
- * @version   7.07
+ * @version   7.08
   */
 
 namespace litepubl\plugins\tidyfilter;
