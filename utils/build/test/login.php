718abdbe065de8d3da057066375a65c0ec55953d
litepubl <yarrowsoft@gmail.com>
2016-09-24 22:23:02 +0300

    add none string to admin language

--- utils/build/test/login.php
+++ utils/build/test/login.php
@@ -16,7 +16,8 @@ $js = json_decode(file_get_contents(__DIR__ . '/tests/_data/admin.json'));
 
 <body>
 <form action="/admin/login/?backurl=<?php
-echo urlencode('/admin/plugins/');
+//echo urlencode('/admin/plugins/');
+echo urlencode('/admin/shop/hosting/');
 ?>" method="post" id="autoform">
 <input type="hidden" name="email" value="<?php echo $js->email; ?>">
 <input type="hidden" name="password" value="<?php echo $js->password; ?>">
