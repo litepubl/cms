83dae1fa07a3f7b3358f31c436dd043c17f7f082
litepubl <yarrowsoft@gmail.com>
2016-10-20 00:11:01 +0300

    tickets tested

--- utils/build/test/tests/acceptance/A20TicketsCest.php
+++ utils/build/test/tests/acceptance/A20TicketsCest.php
@@ -6,13 +6,36 @@ class A20TicketsCest extends \Page\Editor
 {
     use \page\Posts;
 
-    protected $url = '/admin/tickets/';
     protected $url = '/admin/tickets/editor/';
     protected $optionsUrl = '/admin/tickets/options/';
-    private $postsUrl = '/admin/tickets/';
+protected $codeTab = '#tab-1';
+protected $codeEditor = '[name="code"]';
+
+protected function add(): int
+{
+        $this->open();
+$i = $this->tester;
+        $i->wantTo('Create ticket');
+        $this->fillTitleContent(
+            'Some ticket title',
+            'Test withproblem description'
+        );
+
+        $this->screenShot('editor');
+$i->click($this->codeTab);
+$i->waitForElement($this->codeEditor, 1);
+$i->fillField($this->codeEditor, '<?php echo  \'Hello world\';');
+        $this->screenShot('code');
+        $this->submit();
+$id = $this->getPostId();
+$i->openPage($this->getPostLink());
+$this->screenshot('ticket');
+return $id;
+}
 
     protected function test(\AcceptanceTester $i)
     {
+    $this->postsUrl = '/admin/tickets/';
         $i->wantTo('Test tickets plugin');
         $this->installPlugin('tickets');
 
@@ -20,50 +43,22 @@ $i->wantTo('Test tickets options');
 $i->openPage($this->optionsUrl);
         $i->checkOption($this->category);
 $this->screenshot('options');
-$this->submit();
-
-        $this->open();
-        $i->wantTo('Create admin ticket');
-        $i->checkOption($this->category);
-        $this->fillTitleContent(
-            'Declare wiki word',
-            'Some text with [wiki:wikilink] must be here'
-        );
-
-        $this->submit();
-
-        $i->wantTo('Get id post #1');
-        $id1 = $this->getPostId();
-        $holderlink = $this->getPostLink();
-        $this->screenShot('declare');
-
-        $i->openPage($this->url);
-        $i->wantTo('Create post with use wiki word');
-        $i->checkOption($this->category);
-        $this->fillTitleContent(
-            'Use wiki word',
-            'Some text where used [[wikilink]]. Link must be present'
-        );
-
-        $this->submit();
-        $this->screenShot('use');
-
-        $i->wantTo('Get id post #2');
-        $id2 = $this->getPostId();
-
-        $i->wantTo('Check used word');
-        $i->amOnUrl($this->getPostLink());
+        $i->click($this->updateButton);
         $i->checkError();
-        $this->screenShot('used');
-        $i->click($this->wikiLink);
-        $i->checkError();
-        $i->assertEquals($holderlink, $i->getAbsoluteUrl(), 'Wiki word linked');
-        $this->screenShot('declared');
 
-        $i->wantTo('Delete new posts');
-        $this->deletePosts($id1, $id2);
+$id = $this->add();
+        $i->wantTo('Delete new ticket');
+        $this->deletePosts($id);
+$this->logout();
 
+$i->wantTo('Test user ticket');
+$ulogin = $this->getUlogin();
+$ulogin->_click();
+        $i->waitForUrlChanged(10);
+$id = $this->add();
+$this->logout();
+        $i->wantTo('Delete new ticket');
+        $this->deletePosts($id);
         $this->uninstallPlugin('tickets');
-
     }
 }
